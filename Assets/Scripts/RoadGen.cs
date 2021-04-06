af7efa5b306a6947b82ce5897ecd1f0279d07914
TheAus10 <aus10.08@gmail.com>
2021-04-06 14:27:27 -0500

    Generator will now avoid repeats

--- Assets/Scripts/RoadGen.cs
+++ Assets/Scripts/RoadGen.cs
@@ -6,6 +6,7 @@ public class RoadGen : MonoBehaviour
 {
 
     public GameObject[] roadPrefabs;        // holds types of roads
+    public List<GameObject> usedRoads = new List<GameObject>();
     public Vector3 nextCoords;              // starting coordinates of map
     int segmentLength = 505;                // length of 1 segment + 5 for initial length of start segment
     int numSegments = 100;                  // number of segments user wants to place
@@ -17,10 +18,26 @@ public class RoadGen : MonoBehaviour
         // setting start coords
         nextCoords = new Vector3(0, 0, 0);
 
+        // adding one random segment for a specified length
         for (int i = 0; i < numSegments; i++)
         {
+            // copying the array if it is empty
+            if (usedRoads.Count == 0)
+            {
+                for(int j = 0; j < roadPrefabs.Length; j++)
+                {
+                    usedRoads.Add(roadPrefabs[j]);
+                }
+            }
+
+            // getting a random index
+            int index = Random.Range(0, usedRoads.Count);
+
             // creating a new random piece at nextCoords
-            Instantiate(roadPrefabs[Random.Range(0, roadPrefabs.Length)], nextCoords, Quaternion.identity);
+            Instantiate(usedRoads[index], nextCoords, Quaternion.identity);
+
+            // removing it from the list to avoid repeats
+            usedRoads.RemoveAt(index);
 
             // setting position of next segment
             nextCoords.z += segmentLength;
